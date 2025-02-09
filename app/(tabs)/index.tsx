174a62efe5ecdaf1b0f52007b4f2d3644c1dadcd
DINESHWARAN.K <dineshwaran236@gmail.com>
2025-02-09 12:42:37 +0530

    dummy

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -18,7 +18,7 @@ export default function HomeScreen() {
     }
 
     try {
-      const response = await fetch("http://localhost:5000/submit", {
+      const response = await fetch("https://medfam.onrender.com/submit", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
