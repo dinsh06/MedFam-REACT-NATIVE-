0b3ec081702f61cead140d0e0501c1c97275f4f8
DINESHWARAN.K <dineshwaran236@gmail.com>
2025-04-14 19:08:57 +0530

    port configured

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const crypto = require('crypto');
 dotenv.config();
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(express.json()); 
 app.use(cors());  
